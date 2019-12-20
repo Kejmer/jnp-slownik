f8d179d76b1de5cf2446d86b7a243336701e5dc1
dkk <dkk.7575@gmail.com>
2019-12-20 23:49:33 +0100

    Working on node and container.

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -81,9 +81,9 @@ private:
 			// FIXME: Nie ma V()!
 		}
 		
-		node(const K &key,const V &val, node *end) //konstruktor elementu poczatkowego, wskazuje sam na siebie
+		node(const K &key, const V &value, node *end) //konstruktor elementu poczatkowego, wskazuje sam na siebie
 		{
-			this->value = val;
+			this->value = value;
 			this->next = end;
 			this->previous = this;
 			this->key = key;
@@ -177,145 +177,145 @@ private:
 	}
 	
 	
-//public:
-//	class iterator;
-//
-//	insertion_ordered_map() noexcept
-//	{
-//		memory_ptr = shared_ptr<container>(new container());
-//	}
-//
-//	insertion_ordered_map(insertion_ordered_map const &other)
-//	{
-//
-//	}
-//
-//	insertion_ordered_map(insertion_ordered_map &&other)
-//	{
-//
-//	}
-//
-//	~insertion_ordered_map()
-//	{
-//		memory_ptr.reset();
-//	}
-//
-//	insertion_ordered_map &operator=(insertion_ordered_map other)
-//	{
-//
-//	}
-//
-//	bool insert(K const &k, V const &v) noexcept
-//	{
-//		if (this->contains(k)) return false;
-//		has_to_copy();
-//		return memory_ptr->insert(k, v); //powinno zawsze true w tym miejscu
-//	}
-//
-//	void erase(K const &k)
-//	{
-//		if (!this->contains(k)) throw lookup_error();
-//		has_to_copy();
-//		memory_ptr->remove(k);
-//	}
-//
-//	void merge(insertion_ordered_map &other)
-//	{
-//		has_to_copy(); // unless kopiuje siebie w siebie, wtedy nic się nie zmieni
-//		iterator it = other.begin();
-//		iterator fin = other.end()
-//		while (it != fin) {
-//			memory_ptr->insert(it.n->key, it.n->value);
-//			++it;
-//		}
-//	}
-//
-//	V &at(K const &k) //brakuje tej obsłúgi pamięci dzielonej
-//	{
-//		return this->memory_ptr->at(k);
-//	}
-//
-//	V const &at(K const &k) const //tutaj nie potrzeba obsługi pamięci
-//	{
-//		return this->memory_ptr->at(k);
-//	}
-//
-//	V &operator[](K const &k)
-//	{
-//		return this->at(k);
-//	}
-//
-//	size_t size() const
-//	{
-//		return this->memory_ptr->size;
-//	}
-//
-//	bool empty() const
-//	{
-//		return this->size() == 0;
-//	}
-//
-//	void clear() //po prostu przestajemy patrzeć
-//	{
-//		this->memory_ptr.reset(new container());
-//	}
-//
-//	bool contains(K const &k) const
-//	{
-//		this->memory_ptr->contains(k);
-//	}
-//
-//
-//private:
-//
-//	iterator create_iterator(node *n)
-//	{
-//
-//	}
-//
-//public:
-//
-//	class iterator
-//	{
-//	insertion_ordered_map &operator=(insertion_ordered_map other)
-//	private:
-//		node *n;
-//
-//	public:
-//		iterator &operator++()
-//		{
-//			return create_iterator(this->n->next) //co jak next to end?
-//		}
-//
-//		bool operator==(iterator &other)
-//		{
-//			return other.n == this->n
-//		}
-//
-//		bool operator!=(iterator &other)
-//		{
-//			return !(*this == other);
-//		}
-//
-//		V operator*() {
-//			return n->value;
-//		}
-//
-//		friend class insertion_ordered_map;
-//	};
-//
-//	//czy begin i end w środku czy na końcu?
-//
-//
-//	iterator begin()
-//	{
-//		return create_iterator(this->memory_ptr->begin); //przerobić obie funkcje aby nie tworzyły za każdym razem tylko ref zwracały – pamiętać o copy on write
-//	}
-//
-//	iterator end()
-//	{
-//		return create_iterator(this->memory_ptr->end);
-//	}
+public:
+	class iterator;
+
+	insertion_ordered_map() noexcept
+	{
+		memory_ptr = shared_ptr<container>(new container());
+	}
+
+	insertion_ordered_map(insertion_ordered_map const &other)
+	{
+
+	}
+
+	insertion_ordered_map(insertion_ordered_map &&other)
+	{
+
+	}
+
+	~insertion_ordered_map()
+	{
+		memory_ptr.reset();
+	}
+
+	insertion_ordered_map &operator=(insertion_ordered_map other)
+	{
+
+	}
+
+	bool insert(K const &k, V const &v) noexcept
+	{
+		if (this->contains(k)) return false;
+		has_to_copy();
+		return memory_ptr->insert(k, v); //powinno zawsze true w tym miejscu
+	}
+
+	void erase(K const &k)
+	{
+		if (!this->contains(k)) throw lookup_error();
+		has_to_copy();
+		memory_ptr->remove(k);
+	}
+
+	void merge(insertion_ordered_map &other)
+	{
+		has_to_copy(); // unless kopiuje siebie w siebie, wtedy nic się nie zmieni
+		iterator it = other.begin();
+		iterator fin = other.end();
+		while (it != fin) {
+			memory_ptr->insert(it.n->key, it.n->value);
+			++it;
+		}
+	}
+
+	V &at(K const &k) //brakuje tej obsłúgi pamięci dzielonej
+	{
+		return this->memory_ptr->at(k);
+	}
+
+	V const &at(K const &k) const //tutaj nie potrzeba obsługi pamięci
+	{
+		return this->memory_ptr->at(k);
+	}
+
+	V &operator[](K const &k)
+	{
+		return this->at(k);
+	}
+
+	size_t size() const
+	{
+		return this->memory_ptr->size;
+	}
+
+	bool empty() const
+	{
+		return this->size() == 0;
+	}
+
+	void clear() //po prostu przestajemy patrzeć
+	{
+		this->memory_ptr.reset(new container());
+	}
+
+	bool contains(K const &k) const
+	{
+		this->memory_ptr->contains(k);
+	}
+
+
+private:
+
+	iterator create_iterator(node *n)
+	{
+
+	}
+
+public:
+
+	class iterator
+	{
+	insertion_ordered_map &operator=(insertion_ordered_map other);
+	private:
+		node *n;
+
+	public:
+		iterator &operator++()
+		{
+			return create_iterator(this->n->next); //co jak next to end?
+		}
+
+		bool operator==(iterator &other)
+		{
+			return other.n == this->n
+		}
+
+		bool operator!=(iterator &other)
+		{
+			return !(*this == other);
+		}
+
+		V operator*() {
+			return n->value;
+		}
+
+		friend class insertion_ordered_map;
+	};
+
+	//czy begin i end w środku czy na końcu?
+
+
+	iterator begin()
+	{
+		return create_iterator(this->memory_ptr->begin); //przerobić obie funkcje aby nie tworzyły za każdym razem tylko ref zwracały – pamiętać o copy on write
+	}
+
+	iterator end()
+	{
+		return create_iterator(this->memory_ptr->end);
+	}
 	
 };
 
